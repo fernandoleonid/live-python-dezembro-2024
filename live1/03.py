0b1526fd5c7ba5a43420a621d3b507c25350ce4c
professor Fernando <fernandoleonid@gmail.com>
2024-12-03 20:52:42 -0300

    exemplo 3

---
+++ live1/03.py
@@ -0,0 +1,10 @@
+# Um vendedor recebe uma comissão por suas vendas. 
+# Dependendo do valor da venda, a comissão varia. 
+# A comissão é calculada da seguinte forma:
+# - 5% para vendas de até R$1.000,00
+# - 7.5% para vendas entre R$1.000,01 e R$5.000,00
+# - 10% para vendas entre R$5.000,01 e R$10.000,00
+# - 15% para vendas acima de R$10.000,00
+# Escreva um programa em Python que calcule a 
+# comissão do vendedor, dado o valor da venda e o 
+# nome do vendedor.
