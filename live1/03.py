6ee6c96824007c2344ad1d187c9df6a638553409
professor Fernando <fernandoleonid@gmail.com>
2024-12-10 19:32:31 -0300

    solução desafio live1

--- live1/03.py
+++ live1/03.py
@@ -8,3 +8,18 @@
 # Escreva um programa em Python que calcule a 
 # comissão do vendedor, dado o valor da venda e o 
 # nome do vendedor.
+
+nomevendedor = input('Olá, vendedor. digite seu nome: ')
+valorVenda = float(input((f'Olá, {nomevendedor}. Para calcular o valor de sua comissão, digite aqui o valor do produto: ')))
+
+if valorVenda <= 1000.00:
+    comissao = valorVenda * 0.05
+elif valorVenda <= 5000.00:
+    comissao = valorVenda * 0.075
+elif valorVenda <= 10000.00:
+    comissao = valorVenda * 0.10
+else:
+    comissao = valorVenda * 0.15
+
+
+print (f"{nomevendedor}, sua venda no valor de R${valorVenda} terá uma comissão de R${comissao}")
